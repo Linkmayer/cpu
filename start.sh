ad1c323d162ad44a1c71ddcc2b43211abb0876de
Linkmayer <85833134+Linkmayer@users.noreply.github.com>
2021-06-13 15:46:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q9s8ynrtjcrxepnn5zul8edalu45cztr0j5e738.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qp5wqpfepalxuekhkgzr8wgs89mga8up9mgg3gs.test1
 
 
 done
