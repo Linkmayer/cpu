246b67543b137527cb8bcc0a82c9d6ec25ec6f5d
Redawii <85833134+Redawii@users.noreply.github.com>
2021-07-01 20:01:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower  -o stratum+tcps://stratum-eu.rplant.xyz:17029 -u CHfCuBmAoH2uyBq1iTDmF2LEQwkP9sB3zb.A1
-
+./cpuminer-sse2 -a yespowerr16  -o stratum+tcps://stratum-eu.rplant.xyz:13382 -u YQZN8cfaJJCirroLwRshVHwZtAdYiDXS97.A1
 done
